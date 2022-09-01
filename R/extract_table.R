ffc2427bdb073f22d95807e9f001c090f4cddd1c
Richard Hanna <richardshanna91@gmail.com>
2022-09-01 17:31:44 -0400

    Fix package imports

--- R/extract_table.R
+++ R/extract_table.R
@@ -7,7 +7,7 @@
 #' @param .data A REDCapTidieR supertibble provided by \code{read_redcap_tidy()}
 #' @param tbl REDCap table name specification, one max.
 #'
-#' @import checkmate
+#' @importFrom checkmate assert_character
 #' @importFrom rlang .data
 #'
 #' @export
@@ -29,7 +29,7 @@ extract_table <- function(.data,
 
 #' Extract Tables from a REDCapTidieR Supertibble
 #'
-#' Supply a \code{read_redcap_tidy()} output and specify tables of interest to extract. 
+#' Supply a \code{read_redcap_tidy()} output and specify tables of interest to extract.
 #' Users may supply \code{tidyselect} statements such as `starts_with()` or `ends_with()` for easier selection.
 #'
 #' @returns A named list of \code{tibble}s specified by the user.
@@ -37,9 +37,10 @@ extract_table <- function(.data,
 #' @param .data A tidy table provided by \code{read_redcap_tidy()}
 #' @param tbls REDCap table name specifications, one or more.
 #'
-#' @import checkmate
 #' @importFrom rlang .data enquo
+#' @importFrom dplyr select
 #' @importFrom tidyselect eval_select
+#' @importFrom tidyr pivot_wider
 #' @importFrom purrr map pluck
 #'
 #' @export
@@ -49,14 +50,14 @@ extract_tables <- function(.data,
 
   # Extract specified table ----
   # Pass tbls as an expression for enquosure
-  tbls <- rlang::enquo(tbls)
+  tbls <- enquo(tbls)
 
   out <- .data %>%
     select(-.data$structure) %>%
     pivot_wider(names_from = .data$redcap_form_name,
                 values_from = .data$redcap_data)
 
-  out <- out[tidyselect::eval_select(tbls, data = out)]
+  out <- out[eval_select(tbls, data = out)]
 
   out %>%
     map(.f = ~pluck(.)[[1]])
