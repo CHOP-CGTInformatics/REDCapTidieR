6a5dd214718e0e444d7aa76eee895fc56b5ee71f
Ezra Porter <60618324+ezraporter@users.noreply.github.com>
2022-11-02 13:00:13 -0400

    add predefined labels test

--- tests/testthat/test-labelled.R
+++ tests/testthat/test-labelled.R
@@ -9,14 +9,14 @@ test_that("make_labelled applies labels to all elements of supertibble",{
   out <- make_labelled(supertbl)
 
   # Main labels are applied
-  labs <- labelled::var_label(out)
+  main_labs <- labelled::var_label(out)
 
-  expected_labs <- list(
+  expected_main_labs <- list(
     redcap_data = "Data",
     redcap_metadata = "Metadata"
   )
 
-  expect_equal(labs, expected_labs)
+  expect_equal(main_labs, expected_main_labs)
 
   # Labels are applied to both metadata tibbles
   expected_metadata_labs <- list(
@@ -37,6 +37,68 @@ test_that("make_labelled applies labels to all elements of supertibble",{
 
   expect_equal(data_labs1, list(x = "X Label"))
   expect_equal(data_labs2, list(y = "Y Label"))
+
+})
+
+test_that("make_labelled applies all predefined labeles", {
+  # Set up supertibble
+  supertbl <- tibble(
+    redcap_form_name = NA,
+    redcap_form_label = NA,
+    redcap_data = NA,
+    redcap_metadata = NA,
+    redcap_events = NA,
+    structure = NA,
+    data_rows = NA,
+    data_cols = NA,
+    data_size = NA,
+    data_na_pct = NA
+  )
+
+  supertbl$redcap_data <- list(tibble::tribble(
+    ~ redcap_repeat_instance,
+    ~ redcap_event,
+    ~ redcap_arm,
+    ~ form_status_complete
+  ))
+
+  # These won't get predefined labels but need them so we don't error
+  supertbl$redcap_metadata <- list(tibble::tribble(
+    ~ field_name,
+    ~ field_label
+  ))
+
+  out <- make_labelled(supertbl)
+
+  # Check main labs
+  main_labs <- labelled::var_label(out)
+
+  expected_main_labs <- list(
+    redcap_form_name = "Form Name",
+    redcap_form_label = "Form Label",
+    redcap_data = "Data",
+    redcap_metadata = "Metadata",
+    redcap_events = "Events",
+    structure = "Form Structure",
+    data_rows = "# Rows",
+    data_cols = "# Columns",
+    data_size = "Memory Size",
+    data_na_pct = "% NA"
+  )
+
+  expect_equal(main_labs, expected_main_labs)
+
+  # Check data labs
+  data_labs <- labelled::var_label(out$redcap_data[[1]])
+
+  expected_data_labs <- list(
+    redcap_repeat_instance = "Repeat Instance",
+    redcap_event = "Event",
+    redcap_arm = "Arm",
+    form_status_complete = "Form Status"
+  )
+
+  expect_equal(data_labs, expected_data_labs)
 
 })
 
