4a9eefa5f2ac7b96eda791a459236bd95fb16f3a
Ezra Porter <60618324+ezraporter@users.noreply.github.com>
2022-11-07 13:02:49 -0500

    update pre-defined labels + label redcap_events

--- R/labelled.R
+++ R/labelled.R
@@ -5,7 +5,6 @@
 #' \code{read_redcap_tidy()}
 #'
 #' @importFrom dplyr %>%
-#' @importFrom stringr str_replace_all str_to_title
 #' @importFrom purrr map map2
 #' @importFrom rlang check_installed
 #' @importFrom checkmate assert_data_frame
@@ -35,31 +34,50 @@ make_labelled <- function(supertbl) {
 
   # Derive labels ----
   main_labs <- c(
-    redcap_form_name = "Form Name",
-    redcap_form_label = "Form Label",
+    redcap_form_name = "REDCap Instrument Name",
+    redcap_form_label = "REDCap Instrument Description",
     redcap_data = "Data",
     redcap_metadata = "Metadata",
-    redcap_events = "Events",
-    structure = "Form Structure",
-    data_rows = "# Rows",
-    data_cols = "# Columns",
-    data_size = "Memory Size",
-    data_na_pct = "% NA"
+    redcap_events = "Events and Arms Associated with this Instrument",
+    structure = "Repeating or Nonrepeating?",
+    data_rows = "# of Rows in Data",
+    data_cols = "# of Columns in Data",
+    data_size = "Data size in Memory",
+    data_na_pct = "% of Data Missing"
   )
 
-  # Apply formatting to existing metadata fields to derive labels
+  metadata_labs <- c(
+    field_name = "Variable / Field Name",
+    field_label = "Field Label",
+    field_type = "Field Type",
+    section_header = "Section Header",
+    field_note = "Field Note",
+    text_validation_type_or_show_slider_number = "Text Validation Type OR Show Slider Number",
+    text_validation_min = "Text Validation Min",
+    text_validation_max = "Text Validation Max",
+    identifier = "Identifier?",
+    branching_logic = "Branching Logic (Show field only if...)",
+    required_field = "Required Field?",
+    custom_alignment = "Custom Alignment",
+    question_number = "Question Number (surveys only)",
+    matrix_group_name = "Matrix Group Name",
+    matrix_ranking = "Matrix Ranking?",
+    field_annotation = "Field Annotation"
+  )
 
-  # Extract unique set of colnames from metadata tibble rows of supertibble
-  # Edge cases: user removed the metadata field; user changes the name of the metadata field
-  metadata_cols <- map(supertbl$redcap_metadata, colnames) %>%
-    unlist() %>%
-    unique()
+  ## Set some predefined labels for data fields that aren't in the metadata
+  data_labs <- c(
+    redcap_repeat_instance = "REDCap Repeat Instance",
+    redcap_event = "REDCap Event",
+    redcap_arm = "REDCap Arm",
+    form_status_complete = "REDCap Form Status"
+  )
 
-  metadata_labs <- metadata_cols %>%
-    str_replace_all("_", " ") %>%
-    str_to_title()
-
-  names(metadata_labs) <- metadata_cols
+  event_labs <- c(
+    redcap_event = "Event Name",
+    redcap_arm = "Arm Name",
+    arm_name = "Arm Description"
+  )
 
   # Apply labels ----
 
@@ -82,14 +100,6 @@ make_labelled <- function(supertbl) {
 
   # Label cols of each data tibble
 
-  ## Set some predefined labels for data fields that aren't in the metadata
-  predef_labs <- c(
-    redcap_repeat_instance = "Repeat Instance",
-    redcap_event = "Event",
-    redcap_arm = "Arm",
-    form_status_complete = "Form Status"
-  )
-
   out$redcap_data <- map2(
     out$redcap_data,
     out$redcap_metadata,
@@ -99,12 +109,21 @@ make_labelled <- function(supertbl) {
       names(labs) <- .y$field_name
 
       # add pre-defined labels
-      labs <- c(labs, predef_labs)
+      labs <- c(labs, data_labs)
 
       # set them
       safe_set_variable_labels(.x, labs)
     }
   )
+
+  # Label cols of each event tibble if present
+  if ("redcap_events" %in% colnames(supertbl)) {
+    out$redcap_events <- map(
+      out$redcap_events,
+      .f = safe_set_variable_labels,
+      labs = event_labs
+    )
+  }
 
   # Label main cols
   # Do this last since map removes labels from columns we map over
