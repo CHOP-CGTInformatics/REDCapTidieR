a50eb40cb95aab83760c5c67d1b7c084e98518b0
Stephan Kadauke <KADAUKES@EMAIL.CHOP.EDU>
2022-12-24 09:16:55 -0500

    Fix check test

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -113,8 +113,6 @@ test_that("checkmate wrappers work", {
   expect_true(check_arg_choices("a", choices = letters[1:3]))
 
   # token
-  expect_error(check_arg_is_valid_token(123), class = "check_character")
-  expect_error(check_arg_is_valid_token(letters[1:3]), class = "check_character")
-  expect_error(check_arg_is_valid_token("abc"), regexp = "The token is not a valid 32-character hexademical value.")
+  expect_error(check_arg_is_valid_token("abc"), class = "invalid_token")
   expect_true(check_arg_is_valid_token("123456789ABCDEF123456789ABCDEF01"))
 })
