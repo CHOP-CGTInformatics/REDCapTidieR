4e3a7c7b6b94767cc7982c1f8e9c5537299d59bc
Richard Hanna <HANNAR1@EMAIL.CHOP.EDU>
2023-04-05 13:11:52 -0400

    Update tests

--- tests/testthat/test-labelled.R
+++ tests/testthat/test-labelled.R
@@ -315,14 +315,15 @@ test_that("make_labelled preserves S3 class", {
   expect_s3_class(out, "redcap_supertbl")
 })
 
-test_that("make_labelled returns skimr labels", {
-  # Create a tibble with all possible skimr data columns and empty values
-  supertbl_skimr_meta <- skimr::get_default_skimmer_names()
-  supertbl_skimr_meta <- setNames(
-    unlist(supertbl_skimr_meta, use.names=F),
-    rep(names(supertbl_skimr_meta), lengths(supertbl_skimr_meta))
+test_that("make_labelled returns expected skimr labels", {
+  # Create a tibble with all possible skimr data columns
+  skimr_names <- skimr::get_default_skimmer_names()
+  skimr_names <- stats::setNames(
+    unlist(skimr_names, use.names=F),
+    rep(names(skimr_names), lengths(skimr_names))
   )
-  supertbl_skimr_meta <- imap_chr(supertbl_skimr_meta, \(x, idx) paste0(idx, ".", x)) %>%
+
+  supertbl_skimr_meta <- imap_chr(skimr_names, \(x, idx) paste0(idx, ".", x)) %>%
     tibble::as_tibble() %>%
     dplyr::rename("name" = value) %>%
     dplyr::mutate(value = NA) %>%
--- tests/testthat/test-read_redcap.R
+++ tests/testthat/test-read_redcap.R
@@ -537,16 +537,40 @@ test_that("read_redcap metadata contains skimr metrics", {
       ))
   })
 
-  # Check for existence of common skimr metric columns in all
+  # Define common skimr metric column names
   expected_cols <- c(
     "skim_type",
     "n_missing",
     "complete_rate"
   )
 
+  # Get all redcap_metadata names in list form
   redcap_metadata_names <- lapply(out$redcap_metadata, names)
 
+  # Check if expected cols exist in all redcap_metadata_names
   out <- purrr::map(redcap_metadata_names, \(x) all(expected_cols %in% x))
 
   expect_true(all(unlist(out)))
+
+  # Check that if FALSE, no skimr columns appear
+  httptest::with_mock_api({
+    out_no_skimr <-
+      read_redcap(creds$REDCAP_URI,
+                  creds$REDCAPTIDIER_CLASSIC_API,
+                  include_skimr_metadata = FALSE) %>%
+      # suppress expected warning
+      suppressWarnings(classes = c(
+        "field_missing_categories",
+        "empty_parse_warning",
+        "duplicate_labels"
+      ))
+  })
+
+  # Get all redcap_metadata names in list form
+  redcap_metadata_names <- lapply(out_no_skimr$redcap_metadata, names)
+
+  # Check if expected cols exist in all redcap_metadata_names
+  out_no_skimr <- purrr::map(redcap_metadata_names, \(x) all(expected_cols %in% x))
+
+  expect_true(!all(unlist(out_no_skimr)))
 })
