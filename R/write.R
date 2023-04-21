de77e8c3f52ef7c1d2d4898b7b5da119ad098a40
Richard Hanna <HANNAR1@EMAIL.CHOP.EDU>
2023-04-21 14:04:44 -0400

    Add default supertibble and metadata sheets, update tests

--- R/write.R
+++ R/write.R
@@ -4,15 +4,15 @@
 #' Write supertibble outputs to XLSX file outputs, where each REDCap data tibble
 #' exists in a separate sheet.
 #'
-#' @param supertbl a supertibble generated using `read_redcap()`
+#' @param supertbl A supertibble generated using `read_redcap()`
 #' @param labelled Whether or not to include labelled outputs, default `FALSE`.
 #' Requires use of `make_labelled()`.
 #' @param labelled_sheets Whether or not to include labels in the XLSX sheets
 #' instead of raw values. Default `FALSE`.
 #' @param file A character string naming an xlsx file
-#' @param include_supertibble Include a sheet capturing the supertibble output.
+#' @param incl_supertbl Include a sheet capturing the supertibble output.
 #' Default `TRUE`.
-#' @param include_metadata Include a sheet capturing the combined output of the
+#' @param incl_meta Include a sheet capturing the combined output of the
 #' supertibble `redcap_metadata`. Default `TRUE`.
 #' @param tableStyle Any excel table style name or "none" (see "formatting"
 #' vignette in \link[openxlsx]{writeDataTable}). Default "TableStyleLight10".
@@ -39,8 +39,8 @@ write_supertibble_xlsx <- function(supertbl,
                                    labelled = FALSE,
                                    labelled_sheets = FALSE,
                                    file,
-                                   include_supertibble = TRUE,
-                                   include_metadata = TRUE,
+                                   incl_supertbl = TRUE,
+                                   incl_meta = TRUE,
                                    tableStyle = "TableStyleLight10" #nolint: object_name_linter
 ) {
   # Initialize Workbook object ----
@@ -48,6 +48,31 @@ write_supertibble_xlsx <- function(supertbl,
 
   # Assign sheet values based on use of labels
   sheet_vals <- if (labelled_sheets) {supertbl$redcap_form_label} else {supertbl$redcap_form_name}
+
+  if (incl_supertbl) {
+    openxlsx::addWorksheet(wb, sheetName = "supertibble")
+    openxlsx::writeDataTable(wb, sheet = "supertibble",
+                             x = supertbl %>%
+                               mutate(redcap_data = "<tibble>",
+                                      redcap_metadata = "<tibble>"),
+                             startRow = ifelse(labelled, 2, 1),
+                             tableStyle = tableStyle)
+  }
+
+  if (incl_meta) {
+    supertbl_meta <- supertbl %>%
+      select(redcap_metadata) %>%
+      tidyr::unnest(cols = redcap_metadata) %>% # record ID gets duplicated
+      # since no other fields are allowed to be duplicated in REDCap,
+      # can use filtering here for removal of duplicated record ID fields
+      filter(!duplicated(field_name))
+
+    openxlsx::addWorksheet(wb, sheetName = "supertibble_metadata")
+    openxlsx::writeDataTable(wb, sheet = "supertibble_metadata",
+                             x = supertbl_meta,
+                             startRow = ifelse(labelled, 2, 1),
+                             tableStyle = tableStyle)
+  }
 
   # Write all redcap_form_name to sheets
   map(
@@ -66,7 +91,7 @@ write_supertibble_xlsx <- function(supertbl,
 
   # Add labelled features ----
   if (labelled) {
-    add_labelled_xlsx_features(supertbl, wb, sheet_vals)
+    add_labelled_xlsx_features(supertbl, wb, sheet_vals, incl_supertbl, incl_meta)
   }
 
   # Export workbook object
@@ -82,28 +107,75 @@ write_supertibble_xlsx <- function(supertbl,
 #' @param wb An `openxlsx` workbook object
 #' @param sheet_vals Helper argument passed from `write_supertibble_xlsx` to
 #' determine and assign sheet values.
+#' @param incl_supertbl Include a sheet capturing the supertibble output.
+#' Default `TRUE`.
+#' @param incl_meta Include a sheet capturing the combined output of the
+#' supertibble `redcap_metadata`. Default `TRUE`.
 #'
-#' @importFrom purrr map
+#' @importFrom purrr map pluck
 #' @importFrom tidyr pivot_wider
+#' @importFrom dplyr select filter
 #'
 #' @keywords internal
 
-add_labelled_xlsx_features <- function(supertbl, wb, sheet_vals) {
-  # heading 1: Excel's "Explanatory Text" format
+add_labelled_xlsx_features <- function(supertbl,
+                                       wb,
+                                       sheet_vals,
+                                       incl_supertbl = TRUE,
+                                       incl_meta = TRUE) {
+  # Define label header style ----
   label_header_style <- openxlsx::createStyle(fontColour = "#7F7F7F",
                                               textDecoration = "italic",
                                               wrapText = TRUE)
 
-  # Generate variable labels off of labelled dictionary objects
+  # Generate variable labels off of labelled dictionary objects ----
   generate_dictionaries <- function(x) {
     labelled::generate_dictionary(x) %>%
-      dplyr::select("variable", "label") %>%
+      select("variable", "label") %>%
       pivot_wider(
         names_from = "variable",
         values_from = "label"
       )
   }
 
+  # Add supertbl labels ----
+  if (incl_supertbl) {
+    supertbl_labels <- supertbl %>%
+      select(redcap_form_name, redcap_form_label) %>%
+      pivot_wider(names_from = redcap_form_name, values_from = redcap_form_label)
+
+    openxlsx::writeData(wb = wb,
+                        sheet = "supertibble",
+                        x = supertbl_labels, colNames = FALSE)
+
+    openxlsx::addStyle(wb,
+                       sheet = "supertibble",
+                       rows = 1,
+                       cols = seq_along(supertbl_labels),
+                       style = label_header_style)
+  }
+
+  # Add supertbl_meta labels ----
+  if (incl_meta){
+    supertbl_meta_labels <- supertbl %>%
+      select(redcap_metadata) %>%
+      pluck(1,1) %>%
+      labelled::lookfor() %>%
+      select(variable, label) %>%
+      pivot_wider(names_from = "variable", values_from = "label")
+
+    openxlsx::writeData(wb = wb,
+                        sheet = "supertibble_metadata",
+                        x = supertbl_meta_labels, colNames = FALSE)
+
+    openxlsx::addStyle(wb,
+                       sheet = "supertibble_metadata",
+                       rows = 1,
+                       cols = seq_along(supertbl_meta_labels),
+                       style = label_header_style)
+  }
+
+  # Define redcap_data variable labels
   var_labels <- supertbl$redcap_data %>% map(\(x) generate_dictionaries(x))
 
   for (i in seq_along(supertbl$redcap_form_name)) {
--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -1,36 +1,39 @@
-test_that("write_supertibble_xlsx works", {
-  redcap_data_a <- tibble::tribble(
-    ~record_id, ~col_a,
-    1,          "A"
-  )
+redcap_data_a <- tibble::tribble(
+  ~record_id, ~col_a,
+  1,          "A"
+)
 
-  redcap_metadata_a <- tibble::tribble(
-    ~field_name, ~field_label,
-    "record_id",  "Record ID",
-    "col_a",      "Label A"
-  )
+redcap_metadata_a <- tibble::tribble(
+  ~field_name, ~field_label,
+  "record_id",  "Record ID",
+  "col_a",      "Label A"
+)
 
-  redcap_data_b <- tibble::tribble(
-    ~record_id, ~col_b,
-    1,          "B"
-  )
+redcap_data_b <- tibble::tribble(
+  ~record_id, ~col_b,
+  1,          "B"
+)
 
-  redcap_metadata_b <- tibble::tribble(
-    ~field_name, ~field_label,
-    "record_id",  "Record ID",
-    "col_b",      "Label B"
-  )
+redcap_metadata_b <- tibble::tribble(
+  ~field_name, ~field_label,
+  "record_id",  "Record ID",
+  "col_b",      "Label B"
+)
 
-  supertbl <- tibble::tribble(
-    ~redcap_form_name, ~redcap_form_label, ~redcap_data,   ~redcap_metadata,
-    "a",                "A",                redcap_data_a,  redcap_metadata_a,
-    "b",                "B",                redcap_data_b,  redcap_metadata_b
-  ) %>%
-    as_supertbl()
+supertbl <- tibble::tribble(
+  ~redcap_form_name, ~redcap_form_label, ~redcap_data,   ~redcap_metadata,
+  "a",                "A",                redcap_data_a,  redcap_metadata_a,
+  "b",                "B",                redcap_data_b,  redcap_metadata_b
+) %>%
+  as_supertbl()
 
+test_that("write_supertibble_xlsx without labels works", {
   withr::with_dir(
     tempdir(), {
-      write_supertibble_xlsx(supertbl, file = paste0(tempdir(), "supertbl_wb.xlsx"))
+      write_supertibble_xlsx(supertbl,
+                             file = paste0(tempdir(), "supertbl_wb.xlsx"),
+                             incl_meta = FALSE,
+                             incl_supertbl = FALSE)
       sheet_1 <- openxlsx::read.xlsx(xlsxFile = paste0(tempdir(), "supertbl_wb.xlsx"), sheet = 1)
       sheet_2 <- openxlsx::read.xlsx(xlsxFile = paste0(tempdir(), "supertbl_wb.xlsx"), sheet = 2)
 
@@ -40,7 +43,10 @@ test_that("write_supertibble_xlsx works", {
       unlink(paste0(tempdir(), "supertbl_wb.xlsx"))
     }
   )
+})
 
+
+test_that("write_supertibble_xlsx with labels works", {
   labelled_supertbl <- make_labelled(supertbl)
 
   labelled_sheet_1 <- tibble::tribble(
@@ -57,7 +63,11 @@ test_that("write_supertibble_xlsx works", {
 
   withr::with_dir(
     tempdir(), {
-      write_supertibble_xlsx(labelled_supertbl, labelled = TRUE, file = paste0(tempdir(), "labelled_supertbl_wb.xlsx"))
+      write_supertibble_xlsx(labelled_supertbl,
+                             labelled = TRUE,
+                             file = paste0(tempdir(), "labelled_supertbl_wb.xlsx"),
+                             incl_supertbl = FALSE,
+                             incl_meta = FALSE)
       sheet_1 <- openxlsx::read.xlsx(xlsxFile = paste0(tempdir(), "labelled_supertbl_wb.xlsx"), sheet = 1)
       sheet_2 <- openxlsx::read.xlsx(xlsxFile = paste0(tempdir(), "labelled_supertbl_wb.xlsx"), sheet = 2)
 
@@ -67,4 +77,40 @@ test_that("write_supertibble_xlsx works", {
       unlink(paste0(tempdir(), "labelled_supertbl_wb.xlsx"))
     }
   )
+})
+
+test_that("write_supertibble_xlsx has expected supertibble and metadata outputs", {
+  # tribble for readability
+  expected_supertibble <- tibble::tribble(
+    ~redcap_form_name, ~redcap_form_label, ~redcap_data,   ~redcap_metadata,
+    "a",                "A",                "<tibble>",     "<tibble>",
+    "b",                "B",                "<tibble>",     "<tibble>"
+  ) %>%
+    as.data.frame()
+
+  expected_meta <- tibble::tribble(
+    ~field_name, ~field_label,
+    "record_id",  "Record ID",
+    "col_a",      "Label A",
+    "col_b",      "Label B"
+  ) %>%
+    as.data.frame()
+
+  withr::with_dir(
+    tempdir(), {
+      write_supertibble_xlsx(supertbl,
+                             labelled = FALSE,
+                             file = paste0(tempdir(), "default_supertbl_wb.xlsx"),
+                             incl_supertbl = TRUE,
+                             incl_meta = TRUE)
+      sheet_1 <- openxlsx::read.xlsx(xlsxFile = paste0(tempdir(), "default_supertbl_wb.xlsx"), sheet = 1)
+      sheet_2 <- openxlsx::read.xlsx(xlsxFile = paste0(tempdir(), "default_supertbl_wb.xlsx"), sheet = 2)
+
+      expect_equal(sheet_1, expected_supertibble)
+      expect_equal(sheet_2, expected_meta)
+
+      unlink(paste0(tempdir(), "default_supertbl_wb.xlsx"))
+    }
+  )
+
 })
