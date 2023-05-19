497c1f4ba7417310810a579a37a312d0b267854b
Richard Hanna <HANNAR1@EMAIL.CHOP.EDU>
2023-05-19 15:39:19 -0400

    add_skimr_metadata detects and applies labels
    Move skim_date outside of core function

--- R/skimr.R
+++ R/skimr.R
@@ -37,28 +37,49 @@
 add_skimr_metadata <- function(supertbl) {
   check_installed("skimr", reason = "to use `add_skimr_metadata()`")
   check_arg_is_supertbl(supertbl)
-
-  skim_data <- function(redcap_data, redcap_metadata) {
-    excluded_fields <- c(
-      get_record_id_field(redcap_data),
-      "redcap_form_instance", "redcap_event_instance", "redcap_event",
-      "redcap_arm", "form_status_complete"
-    )
-
-    skimmed_data <- redcap_data %>%
-      select(!any_of(excluded_fields)) %>%
-      skimr::skim()
-
-    redcap_metadata %>%
-      left_join(
-        skimmed_data,
-        by = c(field_name = "skim_variable")
-      )
-  }
+  # Check if supertbl has been labelled
+  is_labelled <- is_labelled(supertbl)
 
   supertbl$redcap_metadata <- map2(.x = supertbl$redcap_data,
                                    .y = supertbl$redcap_metadata,
+                                   is_labelled,
                                    .f = skim_data)
 
+  # If supertbl is labelled, apply skimr labels
+  if (is_labelled) {
+    supertbl <- make_labelled(supertbl)
+  }
+
   supertbl
+}
+
+#' @title Apply applicable skimmers to data
+#'
+#' @description
+#' A helper function for `add_skimr_metadata()` which applies applicable
+#' skimmers to a given dataframe.
+#'
+#' @importFrom dplyr left_join select
+#' @importFrom tidyselect any_of
+#'
+#' @returns A dataframe
+#'
+#' @keywords internal
+
+skim_data <- function(redcap_data, redcap_metadata, is_labelled) {
+  excluded_fields <- c(
+    get_record_id_field(redcap_data),
+    "redcap_form_instance", "redcap_event_instance", "redcap_event",
+    "redcap_arm", "form_status_complete"
+  )
+
+  skimmed_data <- redcap_data %>%
+    select(!any_of(excluded_fields)) %>%
+    skimr::skim()
+
+  redcap_metadata %>%
+    left_join(
+      skimmed_data,
+      by = c(field_name = "skim_variable")
+    )
 }
