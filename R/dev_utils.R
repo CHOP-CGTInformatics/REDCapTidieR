fb0213fde7e902c23deb2b30e9f78d0d8710c2c0
Richard Hanna <richardshanna91@gmail.com>
2023-05-31 11:41:53 -0400

    Add release_questions()

--- R/dev_utils.R
+++ R/dev_utils.R
@@ -37,3 +37,22 @@ get_credentials <- function(credentials = NULL, fake = FALSE) {
   names(res) <- creds$name
   res
 }
+
+#' @title Additional release questions
+#'
+#' @description
+#' Additional release questions to be added when using `devtools::release()`
+#' during CRAN submissions.
+#'
+#' @details
+#' This follows the documentation provided in `devtools::release()`.
+#'
+#' @return A series of character string questions
+#'
+#' @keywords internal
+
+release_questions <- function() {
+  c("Have you reviewed all error messages in `cli_message_examples.R`?",
+    "Have you added new error messages in this release to `cli_message_examples.R`?",
+    "Have you tested this release against all test databases in `test_creds.R`?")
+}
