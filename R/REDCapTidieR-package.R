859926fe50465eaa4df55121710f92e440736b22
Richard Hanna <HANNAR1@EMAIL.CHOP.EDU>
2024-07-11 15:57:18 -0400

    Reduce function fixes

--- R/REDCapTidieR-package.R
+++ R/REDCapTidieR-package.R
@@ -5,7 +5,8 @@
 #' expect_factor expect_logical
 #' @importFrom cli cli_abort cli_fmt cli_text cli_vec cli_warn qty
 #' @importFrom dplyr %>% across bind_rows case_when filter group_by if_any if_else
-#' left_join mutate pull recode relocate rename row_number select slice summarise
+#' left_join mutate pull recode relocate rename right_join row_number rowwise
+#' select slice summarise ungroup coalesce cur_column
 #' @importFrom formattable percent
 #' @importFrom lobstr obj_size
 #' @importFrom lubridate is.difftime is.period is.POSIXt is.Date
@@ -15,12 +16,13 @@
 #' @importFrom rlang .data !!! abort as_closure caller_arg caller_env catch_cnd
 #' check_installed cnd_muffle current_call current_env enexpr enquo env_poke
 #' eval_tidy get_env global_env is_atomic is_bare_formula is_bare_list
-#' is_installed new_environment quo_get_expr try_fetch zap as_label
+#' is_installed new_environment quo_get_expr try_fetch zap as_label sym syms
 #' @importFrom stringi stri_split_fixed
 #' @importFrom stringr str_detect str_replace str_replace_all str_squish str_trunc
 #' str_trim str_ends
 #' @importFrom tibble as_tibble is_tibble tibble
-#' @importFrom tidyr complete fill pivot_wider nest unnest unnest_wider
+#' @importFrom tidyr complete fill pivot_wider nest separate_wider_delim unnest
+#' unnest_wider
 #' @importFrom tidyselect all_of any_of ends_with eval_select everything
 #' starts_with where
 #' @importFrom vctrs vec_ptype_abbr vec_ptype
