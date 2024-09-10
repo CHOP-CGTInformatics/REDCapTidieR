7dd592b53eb9cbd9b48a7c4fc9366032df2787c1
Richard Hanna <richardshanna91@gmail.com>
2024-09-10 16:35:02 -0400

    Some updates

--- R/join_data_tibbles.R
+++ R/join_data_tibbles.R
@@ -1,37 +1,26 @@
 join_data_tibbles <- function(suprtbl, x, y, by = NULL, type = "left", name = "new_tibble", suffix = c(".x", ".y")) {
   record_id_field <- get_record_id_field(suprtbl$redcap_data[[1]])
+  join_fn <- get_join_fn(type)
 
   # Append the supertibble with the primary keys column
   suprtbl <- suprtbl |>
     mutate(pks = purrr::map_chr(redcap_data, ~extract_keys(., record_id_field = record_id_field))) %>%
     select(redcap_form_name, redcap_form_label, redcap_data, redcap_metadata, structure, pks)
 
-  tbl_x <- prepare_tibble(suprtbl, x)
-  tbl_y <- prepare_tibble(suprtbl, y)
+  tbl_x <- extract_tibble(suprtbl, x)
+  tbl_x_type <- get_structure(suprtbl, x)
+  tbl_y <- extract_tibble(suprtbl, y)
+  tbl_y_type <- get_structure(suprtbl, y)
 
-  # Define a named list of join functions corresponding to the join types
-  join_functions <- list(
-    left = dplyr::left_join,
-    right = dplyr::right_join,
-    inner = dplyr::inner_join,
-    full = dplyr::full_join
-  )
-
-  # Check if the specified type is valid
-  # TODO: Make a standard check function with cli
-  if (!type %in% names(join_functions)) {
-    stop("Invalid join type. Choose from 'left', 'right', 'inner', or 'full'.")
-  }
+  by <- build_by(suprtbl, x, y)
 
   # Mixed structure requires special handling
-  is_mixed <- any(c(attr(tbl_x, "structure"), attr(tbl_y, "structure")) == "mixed")
+  is_mixed <- any(c(tbl_x_type, tbl_y_type) == "mixed")
 
   if (is_mixed) {
     stop("Mixed structure table detected, this feature is not currently supported.") # TODO: Fix, this is the complicated part
   } else {
-    rlang::exec(
-      join_functions[[type]], tbl_x, tbl_y, by = intersect(attr(tbl_x, "pks"), attr(tbl_y, "pks")), suffix
-    )
+    join_fn(tbl_x, tbl_y, by = by, suffix = suffix)
   }
 }
 
@@ -45,10 +34,36 @@ extract_keys <- function(suprtbl, record_id_field) {
     paste(collapse = ", ")
 }
 
-prepare_tibble <- function(suprtbl, tbl_name) {
+get_structure <- function(suprtbl, tbl_name) {
   tbl <- extract_tibble(suprtbl, tbl = tbl_name)
-  attributes(tbl)$structure <- suprtbl$structure[suprtbl$redcap_form_name == tbl_name]
-  attributes(tbl)$pks <- suprtbl$pks[suprtbl$redcap_form_name == tbl_name] %>%
+  suprtbl$structure[suprtbl$redcap_form_name == tbl_name]
+}
+
+get_join_fn <- function(type) {
+
+  join_functions <- list(
+    left = dplyr::left_join,
+    right = dplyr::right_join,
+    inner = dplyr::inner_join,
+    full = dplyr::full_join
+  )
+
+  # Check if the specified type is valid
+  # TODO: Make a standard check function with cli
+  if (!type %in% names(join_functions)) {
+    stop("Invalid join type. Choose from 'left', 'right', 'inner', or 'full'.")
+  }
+
+  join_functions[[type]]
+}
+
+build_by <- function(suprtbl, x, y) {
+
+  x_pks <- suprtbl$pks[suprtbl$redcap_form_name == x] %>%
     stringr::str_split(", ", simplify = TRUE)
-  return(tbl)
+  y_pks <- suprtbl$pks[suprtbl$redcap_form_name == y] %>%
+    stringr::str_split(", ", simplify = TRUE)
+
+  intersect(x_pks, y_pks)
+
 }
