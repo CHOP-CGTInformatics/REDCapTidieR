5aee69c2a882a77c93afaa111e2d64245503fded
Richard Hanna <richardshanna91@gmail.com>
2025-10-20 10:30:17 -0400

    Simplify

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -373,39 +373,27 @@ test_that("Combining skimr, labelled, and xlsx returns expected snapshot", {
   expect_snapshot(wb_list, cran = FALSE) # Not to be checked on CRAN
 })
 
-test_that("excel_trunc_unique works", {
-  sheet_vals <- c(
-    "A",
-    "Really Really Long Form Name 111",
-    "Really Really Long Form Name 1110",
-    "Really Really Long Form Name 112",
-    "Really Really Long Form Nam.(2)",
-    "Really Really Long Form Name 113",
-    "Really Really Long Form Name 114",
-    "Really Really Long Form Name 115",
-    "Really Really Long Form Name 116",
-    "Really Really Long Form Name 117",
-    "Really Really Long Form Name 118",
-    "Really Really Long Form Name 119"
-  )
+test_that("write_redcap_xlsx handles long sheet names", {
+  long_name_supertbl <- tibble::tribble(
+    ~redcap_form_name, ~redcap_form_label, ~redcap_data, ~redcap_metadata,
+    "a", "Really Really Long Form Name AAA", redcap_data_a, redcap_metadata_a,
+    "b", "Really Really Long Form Name AAB", redcap_data_b, redcap_metadata_b
+  ) %>%
+    as_supertbl()
 
-  expected_out <- c(
-    "A", # Regular cols handled the same
-    "Really Really Long Form Name...", # Initial appended with ...
-    "Really Really Long Form Nam.(3)",
-    "Really Really Long Form Nam.(4)",
-    "Really Really Long Form Nam.(2)", # Position locked
-    "Really Really Long Form Nam.(5)",
-    "Really Really Long Form Nam.(6)",
-    "Really Really Long Form Nam.(7)",
-    "Really Really Long Form Nam.(8)",
-    "Really Really Long Form Nam.(9)",
-    "Really Really Long Form Na.(10)", # Additional magnitudes of characters handled
-    "Really Really Long Form Na.(11)"
-  )
+  withr::with_tempdir({
+    wb_obj_data <- write_redcap_xlsx(long_name_supertbl,
+      file = "long_name_supertbl_wb.xlsx",
+      include_metadata_sheet = FALSE,
+      include_toc_sheet = FALSE,
+      recode_logical = FALSE
+    )
+    sheet_names <- wb_obj_data$sheet_names
+    expected_sheet_names <- c(
+      "Really Really Long Form N...",
+      "Really Really Long Form N...1"
+    )
 
-  expect_warning(sheet_trunc_unique(sheet_vals))
-  out <- suppressWarnings(sheet_trunc_unique(sheet_vals))
-
-  expect_equal(out, expected_out)
+    expect_equal(sheet_names, expected_sheet_names)
+  })
 })
